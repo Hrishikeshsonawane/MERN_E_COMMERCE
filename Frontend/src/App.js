521909b1924177dcd12186042f8723524991c041
Prem <premkambale58@gmail.com>
2023-09-27 20:04:36 +0530

    Added Authentication components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,8 @@ import Header from "./Components/Layout/Header.js"
 import Footer from "../src/Components/Layout/Footer/Footer"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Components/Home/Home.js"
+import Login from "./Components/Auth/Login"
+import Registeration from "./Components/Auth/Registeration"
 
 
 
@@ -13,17 +15,13 @@ function App() {
 
     <Router>
       <Routes>
-            <Route extract path='/' Component={Home} />
-
-            </Routes>
-      <Header />
-      <Footer />
-
+        <Route extract path='/' Component={Home} />
+        <Route extract path='/login' element={<Login />} />
+        {/* <Route extract path='/register' element={<Registeration />} /> */}
+      </Routes>
+      {/* <Header /> */}
+      {/* <Footer /> */}
     </Router>
-
-
-
-
   );
 }
 
