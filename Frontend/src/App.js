d6b3906698bcbc2620550da91eb9b4564d995720
Hrishikesh Sonawane <102814156+Hrishikeshsonawane@users.noreply.github.com>
2023-09-27 20:40:36 +0530

    Header changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import Header from "./Components/Layout/Header.js"
-import Footer from "../src/Components/Layout/Footer/Footer"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Components/Home/Home.js"
 import Login from "./Components/Auth/Login"
@@ -19,8 +17,7 @@ function App() {
         <Route extract path='/login' element={<Login />} />
         {/* <Route extract path='/register' element={<Registeration />} /> */}
       </Routes>
-      {/* <Header /> */}
-      {/* <Footer /> */}
+
     </Router>
   );
 }
