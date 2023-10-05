48b00c40a828d641103edc36eb43fa9020371c21
Prem <pskambale3000@gmail.com.com>
2023-10-06 01:06:50 +0530

    style

--- Frontend/src/Components/Layout/Header.js
+++ Frontend/src/Components/Layout/Header.js
@@ -5,46 +5,47 @@ import { AiOutlineShoppingCart } from "react-icons/ai"
 import { BiSearchAlt2 } from "react-icons/bi"
 import DropDownList from './DropDownList'
 import { useNavigate } from "react-router-dom";
+import searchEmoji from "../../Sources/searchEmoji.gif"
 export default function Header() {
 
- const [isDropdownVisible, setDropdownVisible] = useState(false);
- const [searcText, setSearchText]= useState("")
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [searcText, setSearchText] = useState("")
+
+  const [searchClicked, SetsearchClicked] = useState(true)
+
+
+  const navigate = useNavigate()
 
 
 
 
- const navigate =useNavigate()
-
-
-
- 
 
   const handleMouseEnter = () => {
     setDropdownVisible(true);
   };
 
-  const handleSearch=()=>{
-    
+  const handleSearch = () => {
+    SetsearchClicked(!searchClicked);
   }
 
   const handleMouseLeave = () => {
     setDropdownVisible(false);
   };
 
-  const handleProjectName=()=>{
+  const handleProjectName = () => {
     navigate("/")
   }
 
   const handleSearchProduct = (e) => {
     setSearchText(e.target.value)
     console.log(e.target.value)
-   }
+  }
   const handleShoppingCart = () => {
     navigate("/Cart")
-    }
+  }
 
   const handleProfile = () => {
-   }
+  }
   return (
     <div className='header-main-container'>
       <div className="projectName">
@@ -52,30 +53,47 @@ export default function Header() {
       </div>
 
 
-     <div className="Header_wrapper">
-      <div className="searchProduct">
-        <input type="text"   
-         placeholder="Search products here"
-         value={searcText}
-         onChange={handleSearchProduct}
+      <div className="Header_wrapper">
+        <div className="searchProduct"
+          style={{
+            width: searchClicked ? "46px" : "35vw",
+            height: searchClicked ? "46px" : "46px",
+            borderRadius: searchClicked ? "50%" : "10px",
+            display: searchClicked ? "flex" : "",
+            justifyContent: searchClicked ? "right" : "right",
+            alignItems: searchClicked ? "center" : "",
+            border: searchClicked ? "none" : " none",
+            boxShadow: searchClicked ? "0px 2px 4px rgba(0, 0, 0, 0.2)" : "0px 2px 4px rgba(0, 0, 0, 0.2)",
+            borderRadius: searchClicked ? "50%" : " 10px",
+            overflow: searchClicked ? " hidden" : "hidden"
+          }}
+        >
+          <img src={searchEmoji} className={searchClicked ?  "hided-emoji" : "emoji"} style={{
+            width:"30px",
+            height:"30px"
+          }}/>
+          <input type="text"
+            placeholder="Search products here"
+            value={searcText}
+            onChange={handleSearchProduct}
+            style={{
+              display: searchClicked == true ? "none" : "block",
 
+            }}
+          />
+          <button onClick={handleSearch}><BiSearchAlt2 /></button>
 
+        </div>
+        <div className="shoppingCart">
+          <button onClick={handleShoppingCart}><AiOutlineShoppingCart /> </button>
 
-         
-         /> 
-         <button onClick={handleSearch}><BiSearchAlt2 /></button>
-
-      </div>
-      <div className="shoppingCart">
-        <button onClick={handleShoppingCart}><AiOutlineShoppingCart /> </button>
-
-      </div>
-      <div className='profile'   onMouseEnter={handleMouseEnter}
+        </div>
+        <div className='profile' onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}>
-        <button onClick={handleProfile}><CgProfile /> </button>
-        {isDropdownVisible && <DropDownList />}
+          <button onClick={handleProfile}><CgProfile /> </button>
+          {isDropdownVisible && <DropDownList />}
+        </div>
       </div>
-      </div>
-    </div>
+    </div >
   )
 }
